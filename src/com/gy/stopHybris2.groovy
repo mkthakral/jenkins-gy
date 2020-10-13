6b7bae5372a42c2502ad8df73cc833b8bf842dc7
Mohit <mkthakral@gmail.com>
2020-10-13 16:47:01 +0530

    19 commit

--- src/com/gy/stopHybris2.groovy
+++ src/com/gy/stopHybris2.groovy
@@ -5,8 +5,11 @@ def stopServer(String environment) {
     String osName = new utility().getOS();
     println "OS Name: " + osName
 
-    def request = libraryResource environment + '.properties'
-    println request
+    def propFileContent = libraryResource environment + '.properties'
+    println propFileContent
+    def props = readProperties text: propFileContent
+    println props['hybris-workspace']
+
     Properties props = new Properties()
     File propsFile = new File('./conf/' + environment + '.properties')
     props.load(propsFile.newDataInputStream())
